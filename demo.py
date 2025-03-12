e90acb49cf6549957cfd3894663168f6da40b1b6
maheshrm <maheshrm2002@gmail.com>
2025-03-12 17:32:17 +0530

    added prints

--- demo.py
+++ demo.py
@@ -2,11 +2,14 @@ import sys
 
 def main():
     print("Hello, Mahesh! This is a PyInstaller GitHub Actions demo.")
+    i = 0
+    while i < 50000:
+        print(i)
     if len(sys.argv) > 1:
         print(f"Arguments passed: {sys.argv[1:]}")
     else:
         print("No arguments provided.")
-    input("\nPress Enter to exit...")  # This keeps the window open until you press Enter
+    
 
 
 if __name__ == "__main__":
